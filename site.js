e467edf1780d2a2d8e6662052267922d0f6ad047
Yves Durand <djsaintyves@mac.com>
2026-06-27 19:49:17 +0200

    Localize Mac app page

--- site.js
+++ site.js
@@ -5,7 +5,7 @@
   var translations = [];
   var localeFallbacks = {"en-au":"en-gb","en-ca":"en-gb","fr-ca":"fr","pt-br":"pt-pt"};
   var siteBasePath = "/";
-  var publicPagePaths = ["","support/","privacy/","screenshots/","readme/","choose-vinyl-record/","random-vinyl-record-picker/","manage-vinyl-collection/"];
+  var publicPagePaths = ["","support/","privacy/","screenshots/","readme/","mac-app/","choose-vinyl-record/","random-vinyl-record-picker/","manage-vinyl-collection/"];
   var languageStorageKey = "recordpicker-language";
   var manualLanguageKey = "recordpicker-language-manual";
   var localeMap = {};
